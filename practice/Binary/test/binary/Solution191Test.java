23a5ea22690fcc443ad0bb8273f7f3295374a27d
LynnHaDo <do24l@mtholyoke.edu>
2024-06-24 23:05:02 +0700

    Add Leetcode DP

---
+++ practice/Binary/test/binary/Solution191Test.java
@@ -0,0 +1,30 @@
+package practice.Binary.test.binary;
+
+import org.junit.Test;
+
+import practice.Binary.src.binary.Solution191;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution191Test {
+    @Test 
+    public void test() {
+        Solution191 solution = new Solution191();
+
+        assertEquals(3, solution.hammingWeight(11));
+    }
+
+    @Test 
+    public void test1() {
+        Solution191 solution = new Solution191();
+
+        assertEquals(1, solution.hammingWeight(128));
+    }
+
+    @Test 
+    public void test2() {
+        Solution191 solution = new Solution191();
+
+        assertEquals(30, solution.hammingWeight(2147483645));
+    }
+}
