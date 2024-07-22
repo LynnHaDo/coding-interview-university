b9158e49ff35c41b749d839c0dff11de81ace95a
LynnHaDo <do24l@mtholyoke.edu>
2024-07-23 00:27:08 +0700

    Add leetcode 105 (build tree)

--- practice/BST/src/leetcode/src/Solution105.java
+++ practice/BST/src/leetcode/src/Solution105.java
@@ -1,8 +1,34 @@
 package leetcode.src;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution105 {
+    private TreeNode recursivelyBuildTree(int[] preorder, int[] inorder, int preorderStart, int preorderEnd,
+            int inorderStart, int inorderEnd) {
+        if (preorderEnd - preorderStart + 1 <= 0 || inorderEnd - inorderStart + 1 <= 0) {
+            return null;
+        }
+
+        TreeNode center = new TreeNode(preorder[preorderStart]);
+        // Find the cutoff that divides the left and right subtrees
+        int mid = inorderStart;
+        for (;; mid++) {
+            if (inorder[mid] == center.val) {
+                break;
+            }
+        }
+
+        // Build left subtree
+        center.left = recursivelyBuildTree(preorder, inorder, 1, mid - 1, 0, mid -1);
+        center.right = recursivelyBuildTree(preorder, inorder, mid+1, preorder.length - 1, mid+1, inorder.length);
+        return center;
+    }
+
     /**
-     * Construct and return the binary tree based on preorder and inorder traversal results
+     * Construct and return the binary tree based on preorder and inorder traversal
+     * results
      * 
      * Constraints:
      * 
@@ -14,11 +40,29 @@ public class Solution105 {
      * inorder is guaranteed to be the inorder traversal of the tree.
      * 
      * @param preorder preorder traversal of a binary tree (self, left, right)
-     * @param inorder inorder traversal of the same tree (left, self, right)
+     * @param inorder  inorder traversal of the same tree (left, self, right)
      * @return root of the tree
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
-        return null;
+        // Root is the first element in preorder array
+        TreeNode root = new TreeNode(preorder[0]);
+
+        // Tree with 1 node
+        if (preorder.length == 1) {
+            return root;
+        }
+
+        // Find the cutoff that divides the left and right subtrees
+        int mid = 0;
+        for (;; mid++) {
+            if (inorder[mid] == root.val) {
+                break;
+            }
+        }
+
+        // Build left subtree
+        root.left = recursivelyBuildTree(preorder, inorder, 1, mid - 1, 0, mid -1);
+        root.right = recursivelyBuildTree(preorder, inorder, mid+1, preorder.length - 1, mid+1, inorder.length);
+        return root;
     }
 }
