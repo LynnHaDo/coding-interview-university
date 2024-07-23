bb76bccf902b3193c7ba738140a8ac27a45f2f69
LynnHaDo <do24l@mtholyoke.edu>
2024-07-23 11:59:41 +0700

    Add isSubtree method

--- practice/BST/src/leetcode/src/Solution572.java
+++ practice/BST/src/leetcode/src/Solution572.java
@@ -1,7 +1,41 @@
 package leetcode.src;
 
 public class Solution572 {
+    private boolean isSubtreeRecursive(TreeNode root, TreeNode subRoot, boolean isRootFound) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+
+        if (root == null && subRoot != null) {
+            return false;
+        }
+
+        if (root != null && subRoot == null) {
+            return isRootFound;
+        }
+
+        if (subRoot.val == root.val) {
+            boolean isLeftSubtree = isSubtreeRecursive(root.left, subRoot.left, true);
+            boolean isRightSubtree = isSubtreeRecursive(root.right, subRoot.right, true);
+
+            return isLeftSubtree && isRightSubtree;
+        }
+
+        return isSubtreeRecursive(root.left, subRoot, false) 
+            || isSubtreeRecursive(root.right, subRoot, false);
+    }
+    /**
+     * Given the roots of two binary trees root and subRoot, 
+     * 
+     * A subtree of a binary tree tree is a tree that consists 
+     * of a node in tree and all of this node's descendants. 
+     * The tree tree could also be considered as a subtree of itself.
+     * 
+     * @param root root of the original tree
+     * @param subRoot root of the subtree to check for
+     * @return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise
+     */
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        return false;
+        return isSubtreeRecursive(root, subRoot, false);
     }
 }
--- practice/BST/src/leetcode/test/Solution572Test.java
+++ practice/BST/src/leetcode/test/Solution572Test.java
@@ -1,5 +1,48 @@
 package leetcode.test;
 
+import leetcode.src.Solution572;
+import leetcode.src.TreeNode;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class Solution572Test {
-    
+    Solution572 solution;
+    TreeNode root1, subtree, root2;
+    TreeNode root3, subtree3;
+
+    @Before 
+    public void setup() {
+        this.solution = new Solution572();
+        subtree = new TreeNode(4, new TreeNode(1), new TreeNode(2));
+    }
+
+    @Test 
+    public void test1() {
+        // root = [3,4,5,1,2], subRoot = [4,1,2]
+        // Output: true
+       root1 = new TreeNode(3, new TreeNode(4), new TreeNode(5));
+       root1.getLeft().setLeft(new TreeNode(1));
+       root1.getLeft().setRight(new TreeNode(2));
+       assertTrue(solution.isSubtree(root1, subtree));
+    }
+
+    @Test 
+    public void test2() {
+        // root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2] 
+        // Output: false
+        root2 = new TreeNode(3, new TreeNode(4), new TreeNode(5));
+        root2.getLeft().setLeft(new TreeNode(1));
+        root2.getLeft().setRight(new TreeNode(2));
+        root2.getLeft().getRight().setRight(new TreeNode(0));
+
+        assertFalse(solution.isSubtree(root2, subtree));
+    }
+
+    @Test 
+    public void test3() {
+        assertTrue(solution.isSubtree(new TreeNode(1, new TreeNode(1), null), 
+                    new TreeNode(1)));
+    }
 }
